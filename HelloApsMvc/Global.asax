090e8e0fbeaa8b337be8682cab36612ec5279744
Jorg Jenni <joerg.jenni@garaio.com>
2014-11-29 14:25:03 +0000

    initial commit

---
+++ HelloApsMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HelloApsMvc.MvcApplication" Language="C#" %>
---
+++ HelloApsMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelloApsMvc.Startup))]
+namespace HelloApsMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
